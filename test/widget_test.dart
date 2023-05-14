20b1197cdcb8f50aab3121a7dbf335d16fed50a1
Danyl Brahin <inicerer141@gmail.com>
2023-05-14 20:37:55 +0300

    Remove unused import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hikki_enciclopedia/main.dart';
 import 'package:hikki_enciclopedia/presentation/app.dart';
 
 void main() {
